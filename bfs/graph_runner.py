17f7a5e8bedbe1135d3e8ddb80b8324a7244fd36
Alex Steenson <alexsteenson@decoded.com>
2018-07-02 16:33:09 +0100

    BFS searches for a goal on a grid and prints grid at each iteration

--- bfs/graph_runner.py
+++ bfs/graph_runner.py
@@ -21,15 +21,43 @@ def breadth_first_search_1(graph, start):
                 explored[next_node] = True
 
 
-# Create a sample graph
-example_graph = SimpleGraph()
-example_graph.edges = {
-    'A': ['B'],
-    'B': ['A', 'C', 'D'],
-    'C': ['A'],
-    'D': ['E', 'A'],
-    'E': ['B', 'F'],
-    'F': []
-}
+def breadth_first_search_2(graph, start, end):
+    # return "explored"
+    unexplored = Queue()
+    unexplored.put(start)
+    explored = {}
+    explored[start] = None
 
-breadth_first_search_1(example_graph, 'C')
+    while not unexplored.empty():
+        current = unexplored.get()
+        if current == end:
+            return explored
+        for next_node in graph.neighbors(current):
+            if next_node not in explored:
+                unexplored.put(next_node)
+                explored[next_node] = current
+        g.draw_grid(current, explored, start, end)
+        sys.stdout.write("\n")
+
+    return explored
+
+
+# # Create a sample graph
+# example_graph = SimpleGraph()
+# example_graph.edges = {
+#     'A': ['B'],
+#     'B': ['A', 'C', 'D'],
+#     'C': ['A'],
+#     'D': ['E', 'A'],
+#     'E': ['B', 'F'],
+#     'F': []
+# }
+#
+# breadth_first_search_1(example_graph, 'C')
+
+g = SquareGrid(30, 15)
+#g.walls = DIAGRAM1_WALLS
+
+parents = breadth_first_search_2(g, (8, 7), (25, 11))
+# Visited, Start, End
+g.draw_grid((-1, -1), parents, (8, 7), (25, 11))
--- map/maps.py
+++ map/maps.py
@@ -1,6 +1,48 @@
+import sys
+
+
 class SimpleGraph:
     def __init__(self):
         self.edges = {}
 
     def neighbors(self, node_id):
         return self.edges[node_id]
+
+
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+
+    def in_bounds(self, node_id):
+        (x, y) = node_id
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, node_id):
+        return node_id not in self.walls
+
+    def neighbors(self, node_id):
+        (x, y) = node_id
+        results = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
+        if (x + y) % 2 == 0: results.reverse()  # aesthetics
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+    def draw_grid(self, current, visited, start, end):
+        for y in range(self.height):
+            for x in range(self.width):
+                draw_node = (x, y)
+
+                if draw_node == current:
+                    sys.stdout.write(" C ")
+                elif draw_node == start:
+                    sys.stdout.write(" S ")
+                elif draw_node == end:
+                    sys.stdout.write(" E ")
+                elif draw_node in visited:
+                    sys.stdout.write(" V ")
+                else:
+                    sys.stdout.write(" . ")
+            sys.stdout.write("\n")
